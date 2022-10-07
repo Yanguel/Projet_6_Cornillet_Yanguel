008acf267b1b462c36bcd5654045db9f127dbba0
yanguel <yanguelcornillet@yahoo.com>
2022-10-07 20:06:59 +0200

    Modification de nom

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -5,11 +5,11 @@ const fs = require("fs");
 // et de l'afficher, tout en gérant les images
 exports.createThing = (req, res, next) => {
   console.log(req.body);
-  const thingObject = JSON.parse(req.body.sauce);
-  delete thingObject._id;
-  delete thingObject._userId;
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  delete sauceObject._userId;
   const sauce = new Sauce({
-    ...thingObject,
+    ...sauceObject,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
@@ -26,15 +26,25 @@ exports.createThing = (req, res, next) => {
 };
 // Permet de mettre à jour un Schéma déja existant
 exports.modifyThing = (req, res, next) => {
-  const thingObject = req.file
-    ? {
-        ...JSON.parse(req.body.thing),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.filename
-        }`,
-      }
-    : { ...req.body };
-  delete thingObject._userId;
+  console.log(req.params.id);
+  const sauceObject =
+    // Une image existe elle ?
+    req.file
+      ? // Si la modification contient une image =>
+        // Nous récuperont l'objet en parsant la chaine de caractère et recréant l'URL de l'image
+        {
+          ...JSON.parse(req.body.sauce),
+          imageUrl: `${req.protocol}://${req.get("host")}/images/${
+            req.file.filename
+          }`,
+        }
+      : // Si il n'y a pas d'objet de transmit alors recuperer directement dans le corps de la requete
+        { ...req.body };
+
+  //Supprime le userId pour éviter les fraudes
+  delete sauceObject._userId;
+
+  // Rechercher l'objet dans la base de donnée pour récuperer et vérifier
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       // Si l'userId est différent de l'userId du token
@@ -45,13 +55,14 @@ exports.modifyThing = (req, res, next) => {
       else {
         Sauce.updateOne(
           { _id: req.params.id },
-          { ...thingObject, _id: req.params.id }
+          { ...sauceObject, _id: req.params.id }
         )
           .then(() => res.status(200).json({ message: "Objet modifié!" }))
           .catch((error) => res.status(401).json({ error }));
       }
     })
     .catch((error) => {
+      console.log("test ", req.body);
       res.status(400).json({ error });
     });
 };
@@ -76,14 +87,12 @@ exports.deleteThing = (req, res, next) => {
       res.status(500).json({ error });
     });
 };
-
 // Récuperer un schéma en particulier
 exports.getOneThing = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => res.status(200).json(sauce))
     .catch((error) => res.status(404).json({ error }));
 };
-
 // Recuperer tout les objets
 exports.getAllThing = (req, res, next) => {
   Sauce.find()
